a5c12228301b64485fe5b873de476a768b6530c5
ElSiayer <jf.simba.97@hotmail.com>
2022-05-14 10:50:53 -0500

    Agregacion de clases

---
+++ Assets/Scripts/Answer.cs
@@ -0,0 +1,13 @@
+public class Answer
+{
+    public Answer(int id, string text, bool isCorrect)
+    {
+        this.id = id;
+        this.text = text;
+        this.isCorrect = isCorrect;
+    }
+    public int id { get; set; }
+    public string text { get; set; }
+    public bool isCorrect { get; set; }
+    
+}
---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,6 @@
+using System.Collections.Generic;
+public class Question
+{
+    public string text { get; set; }
+    public List<Answer> options { get; set; }
+}
